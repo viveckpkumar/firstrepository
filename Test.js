68f6db3be5cdd9131b8e79bb35315df95f386fa9
Viveck P Kumar <viveckpkumar@vivecks-mbp.lan>
2020-05-09 13:02:51 +0800

    Initial commit

---
+++ Test.js
@@ -0,0 +1,17 @@
+function isprime(n)
+{
+    for(var i=2;i<n;i++)
+    {
+        if (n%i===0) {
+            console.log(n,"is not prime");
+            break;
+        }
+        else if (i==n-1&n%i!=0)
+        {
+            console.log(n,"is a prime no")
+        }
+        else continue;
+    }
+}
+
+isprime(991);
---
+++ test2.js
@@ -0,0 +1,10 @@
+function iseven(a)
+{
+    if(a%2==0)
+    {
+        console.log(a,"Its an even no");
+    }
+    else console.log(a,"Its an odd no");
+}
+
+iseven(7);
